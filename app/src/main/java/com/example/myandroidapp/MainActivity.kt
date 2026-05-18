196d63e693c2f10b43ed59ac876ee4bc73b570a6
Rodrigo <rodrigo_103_103@hotmail.com>
2026-05-18 20:10:11 -0300

    Enable edge-to-edge support in MainActivity
    Call `enableEdgeToEdge()` before `setContent` to allow the application to display content behind the system bars.

--- app/src/main/java/com/example/myandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/myandroidapp/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge()
         setContent {
             MyAndroidAppTheme {
                 Surface(
